930066eddf9a34fe998c1c2c6559bc464be2a9f6
Rohan Shrivastava <rohansh0808@gmail.com>
2024-06-21 11:09:40 +0530

    Add files via upload

---
+++ Day 11/index.js
@@ -0,0 +1,11 @@
+export const countVowels = (str) => {
+	// Write your code here
+	const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
