a3f381b862cc65ab1eceda5bed30302b45f5d638
Rohan Shrivastava <rohansh0808@gmail.com>
2024-06-28 16:59:08 +0530

    Add files via upload

---
+++ Day 18/index.js
@@ -0,0 +1,14 @@
+// Create your product object
+const product = {
+	id: 1, // numeric value for id
+	name: 'Laptop', // non-empty string for name
+	price: 999.99, // numeric value for price
+	availability: true // boolean value for availability
+}
+
+// export your product object
+export { product }
+
+// console.log the object to view the output
+console.log(product)
+// TIP: Use the run button to run your JS Code
