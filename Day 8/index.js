3fe60b776b0199177f1c06333e4932a2a9877856
Rohan Shrivastava <rohansh0808@gmail.com>
2024-07-01 10:00:42 +0530

    Update index.js

--- Day 8/index.js
+++ Day 8/index.js
@@ -1,15 +1,11 @@
 export const sumOfThirds = (arr) => {
 	// Write your code here
 	// Check if the array has less than 3 elements
-    if (arr.length < 3) {
-        return 0;
-    }
-
-    // Filter the elements at every third index and sum them up
-    const sum = arr
-        .filter((_, index) => index % 3 === 0)
-        .reduce((acc, num) => acc + num, 0);
-
-    return sum;
+  	let sum = 0
+	for (let i = 0; i < arr.length; i += 3) {
+		sum += arr[i]
+	}
+	return sum
 }
+
 
