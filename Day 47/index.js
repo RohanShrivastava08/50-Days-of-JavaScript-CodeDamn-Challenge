06c504ef81ad0df42dc5513d2465fa06e24b1f64
Rohan Shrivastava <rohansh0808@gmail.com>
2024-07-28 10:55:49 +0530

    Add files via upload

---
+++ Day 47/index.js
@@ -0,0 +1,8 @@
+// Implement your try-catch block here
+try {
+    // Code that might throw an error
+    throw new Error('myCustomError: This is a custom error message');
+} catch (error) {
+    // Handling the error
+    console.error(error);
+}
