baae9cd35e212e5e8088d448c57e4f59671427ce
Rohan Shrivastava <rohansh0808@gmail.com>
2024-07-29 10:39:47 +0530

    Add files via upload

---
+++ Day 48/index.js
@@ -0,0 +1,12 @@
+// Create your functions here
+
+
+// Function to add two numbers
+export function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  // Function to multiply two numbers
+  export default function multiplyNumbers(a, b) {
+    return a * b;
+  }
